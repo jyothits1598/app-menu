3f73c6d0c11ae007d4ddfa064269f973141bb1d4
Ganesh S Palankar <ganeshpalankar01@gmail.com>
2020-07-23 08:42:46 +0530

    completed menus tab integration, testing and some design needed

--- src/app/views/dashboard/restaurant-menu/restaurant-menu-menus/restaurant-menu-menus.component.ts
+++ src/app/views/dashboard/restaurant-menu/restaurant-menu-menus/restaurant-menu-menus.component.ts
@@ -1,31 +1,45 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
-import { Observable } from 'rxjs/internal/Observable';
+import { ActivatedRoute, ParamMap, Router, NavigationEnd, RouterEvent } from '@angular/router';
 import { StoreService } from 'src/app/services/store.service';
 import { RestApiService } from 'src/app/services/rest-api.service';
 import { StoreMenu, StoreMenuTime } from 'src/app/_models/store-menu';
+import { filter } from 'rxjs/operators';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-restaurant-menu-menus',
   templateUrl: './restaurant-menu-menus.component.html',
   styleUrls: ['./restaurant-menu-menus.component.scss']
 })
-export class RestaurantMenuMenusComponent implements OnInit , OnDestroy{
-  menus : Array<StoreMenu>= [];
-  
+export class RestaurantMenuMenusComponent implements OnInit, OnDestroy {
+  menus: Array<StoreMenu> = [];
+  routerSub$ : Subscription;
   constructor(
     public route: ActivatedRoute
+    , private router: Router
     , private storeService: StoreService
     , private restApiService: RestApiService
-  ) { }
+  ) {
+    
+    this.routerSub$ = this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd && this.route.children.length == 0)
+    ).subscribe((event) => {
+      this.fetchMenus();
+    });
 
-  ngOnDestroy(): void {
   }
 
-  fetchMenus(){
+  ngOnDestroy(): void {
+    this.routerSub$.unsubscribe();
+  }
+
+  fetchMenus() {
     this.menus = [];
-    this.restApiService.getData(`store/menus/availability/get/${this.storeService.activeStore}/all`, (response)=>{
-      if(response['data'] && response['data'].length > 0){
+    
+    if(!this.storeService.activeStore) return this.router.navigate(['../notfound'], {relativeTo: this.route});
+
+    this.restApiService.getData(`store/menus/availability/get/${this.storeService.activeStore}/all`, (response) => {
+      if (response['data'] && response['data'].length > 0) {
         let data = response['data'];
         data.forEach(menu => {
           let newMenu = new StoreMenu();
@@ -39,7 +53,7 @@ export class RestaurantMenuMenusComponent implements OnInit , OnDestroy{
   }
 
   ngOnInit(): void {
-    this.fetchMenus();
+
   }
 
 }
