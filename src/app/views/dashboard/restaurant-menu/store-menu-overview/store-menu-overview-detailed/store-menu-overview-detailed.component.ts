9b482f42acec86425d13a38ac4e7d0fb486f0f31
Basavakumar <vishwanatha.st@sanradiance.com>
2020-08-17 11:00:00 +0530

    overview page design

--- src/app/views/dashboard/restaurant-menu/store-menu-overview/store-menu-overview-detailed/store-menu-overview-detailed.component.ts
+++ src/app/views/dashboard/restaurant-menu/store-menu-overview/store-menu-overview-detailed/store-menu-overview-detailed.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ActivatedRoute, Router } from '@angular/router';
+import { RestApiService } from 'src/app/services/rest-api.service';
+import { StoreService } from 'src/app/services/store.service';
+import { AlertService } from 'src/app/services/alert.service';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-store-menu-overview-detailed',
@@ -11,11 +15,18 @@ export class StoreMenuOverviewDetailedComponent implements OnInit {
   dropdownChild = true;
   right_image:string = "../../../assets/images/ico_right.png";
   down_image:string = "../../../assets/images/ico_down.png";
+  imageUrl: string = null;
+  errors: string;
+  fileUptoLoad: File;
 
   constructor(
     private _modalService: NgbModal,
     public route: ActivatedRoute,
     private router: Router,
+    private restApiService: RestApiService,
+    private storeService: StoreService,
+    private alertService: AlertService,
+    private dataService: DataService
   ) { }
 
   ngOnInit(): void {
@@ -27,5 +38,54 @@ export class StoreMenuOverviewDetailedComponent implements OnInit {
 
   toggleClass() {
     this.dropdownChild = !this.dropdownChild;
+  }
+  openVerticallyCentered(content) {
+    this.modalService.open(content, { centered: true });
+  }
+
+  opencategoryCentered(category) {
+    this.modalService.open(category, { centered: true });
+  }
+
+  openmodifierCentered(modifier) {
+    this.modalService.open(modifier, { centered: true });
+  }
+
+  onFileChanged(event) {
+    this.fileUptoLoad = event.target.files[0];
+    if (this.fileUptoLoad) {
+      if (!this.dataService.validateFileSize(this.fileUptoLoad.size)) {
+        this.alertService.showNotification('File to be uploaded should be less than 5MB', 'error')
+        return false;
+      }
+      if (!this.dataService.validateFileExtension(this.fileUptoLoad.name)) {
+        this.alertService.showNotification('Selected file format is not supported', 'error')
+        return false;
+      }
+      let reader = new FileReader();
+      reader.readAsDataURL(this.fileUptoLoad);
+
+      reader.onload = (e: any) => {
+        var img = new Image();
+        img.src = e.target.result;
+        img.onload = () => {
+          if (img.width < 500 || img.height < 500) {
+            this.alertService.showNotification('Minimum size 500*500 pixel', 'error')
+            return false;
+          }
+          let form_data = new FormData();
+          form_data.append('item_image', this.fileUptoLoad);
+          this.alertService.showLoader();
+          this.restApiService.pushSaveFileToStorageWithFormdata(form_data, 'store/items/upload/image', (response) => {
+            if(response && response['success'] && response['data']) { 
+              this.imageUrl=response['data'];
+            this.alertService.hideLoader();
+            }
+          });
+        }
+      }
+    } else {
+        this.alertService.showNotification('No file selected', 'error');
+    }
   }
 }
