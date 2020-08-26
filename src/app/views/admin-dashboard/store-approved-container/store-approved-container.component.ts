21d3b7c4856614022f2882faca513ce21ad6b6a2
Basavakumar <vishwanatha.st@sanradiance.com>
2020-08-26 20:44:18 +0530

    ui changes

---
+++ src/app/views/admin-dashboard/store-approved-container/store-approved-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-store-approved-container',
+  templateUrl: './store-approved-container.component.html',
+  styleUrls: ['./store-approved-container.component.scss']
+})
+export class StoreApprovedContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/views/admin-dashboard/store-approved-list/store-approved-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-store-approved-list',
+  templateUrl: './store-approved-list.component.html',
+  styleUrls: ['./store-approved-list.component.scss']
+})
+export class StoreApprovedListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
