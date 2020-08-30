e25d3f2bca8917eb02f91ca8fe73e98fab0879a5
gangajogur <ganga.jogur@gmail.com>
2020-08-30 22:24:18 +1000

    added more code review comments

--- src/app/views/add-store-forms/fifth-forms/fifth-forms.component.ts
+++ src/app/views/add-store-forms/fifth-forms/fifth-forms.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { RestApiService } from 'src/app/services/rest-api.service';
 import { AlertService } from 'src/app/services/alert.service';
 import { AuthenticationService } from 'src/app/services/authentication.service';
@@ -11,7 +11,7 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
   styleUrls: ['./fifth-forms.component.scss']
 })
 export class FifthFormsComponent implements OnInit {
-  store_id:string;
+  store_id: string;
   storename: string;
   active_status = false;
   returnUrl: string;
@@ -22,38 +22,45 @@ export class FifthFormsComponent implements OnInit {
     private alertService: AlertService,
     private route: ActivatedRoute,
     private authenticateService: AuthenticationService
-  ) { 
+  ) {
     this.store_id = this.route.snapshot.paramMap.get('store-id');
   }
 
   ngOnInit(): void {
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '';
     var obj = this;
-    if(localStorage.getItem('Audit_Auth') && localStorage.getItem('loggedUser')){
+    if (localStorage.getItem('Audit_Auth') && localStorage.getItem('loggedUser')) {
       obj.authenticateService.checkExpiryStatus();
     }
   }
 
   storeDetail() {
     this.alertService.showLoader();
-    this.restapiService.getData('store/get',(response)=>{
-      if(response && response['success'] && response['data'] && Array.isArray(response['data']) && response['data'].length > 0){
+    // Suggest have strongly typed objects for response and access properties.
+    // e.g. reponse.success and response.data like below
+    this.restapiService.getData('store/get', response => {
+      if (
+        response &&
+        response.success &&
+        response.data &&
+        Array.isArray(response['data']) &&
+        response['data'].length > 0
+      ) {
         response['data'].forEach(element => {
-          if(element.store_id) {
+          if (element.store_id) {
             this.storename = element.store_name;
-            if(element['active_flag'] == 0 && element['next_step'] == "") {
-              return this.router.navigateByUrl('/pending-approval');              
-            } else if(element['active_flag'] == 1) {
+            if (element['active_flag'] == 0 && element['next_step'] == '') {
+              return this.router.navigateByUrl('/pending-approval');
+            } else if (element['active_flag'] == 1) {
               return this.router.navigateByUrl('/approval');
-            } 
+            }
             // else if(element['active_flag'] == 0 && element['next_step']) {
             //   return this.router.navigateByUrl('/')
             // }
-          }  
+          }
           this.alertService.hideLoader();
-        });       
+        });
       }
     });
   }
-
 }
--- src/app/views/signup/signup.component.ts
+++ src/app/views/signup/signup.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { HttpClient,HttpHeaders } from '@angular/common/http';
-import { ReactiveFormsModule,FormsModule,FormGroup,FormControl,Validators,FormBuilder } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {
+  ReactiveFormsModule,
+  FormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
 import { Subject } from 'rxjs';
 import { MustMatch } from 'src/app/_helpers/must-match.validator';
 import { REQUEST_A_ACTIVE } from 'src/environments/environment';
@@ -36,8 +43,10 @@ export class SignupComponent implements OnInit {
     private restApiservice: RestApiService,
     private alertservice: AlertService,
     private authenticateService: AuthenticationService
-  ) { }
-    
+  ) {}
+
+  // Instead of this method consider using PatternValidator from Angular
+  // https://angular.io/api/forms/PatternValidator
   //only number will be add
   keyPress(event: any) {
     const pattern = /[0-9\+\-\ ]/;
@@ -46,43 +55,49 @@ export class SignupComponent implements OnInit {
     if (event.keyCode != 8 && !pattern.test(inputChar)) {
       event.preventDefault();
     }
-
   }
 
   ngOnInit(): void {
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '';
     var obj = this;
-    if(localStorage.getItem('Audit_Auth') && localStorage.getItem('loggedUser')){
+    if (localStorage.getItem('Audit_Auth') && localStorage.getItem('loggedUser')) {
       this.router.navigateByUrl('/dashboard');
       //obj.authenticateService.checkExpiryStatus();
     }
-    this.signupMenuzappform = this.formBuilder.group({
-      fname: ['', Validators.required],
-      lname: ['', Validators.required],
-      signupemail: [null, [Validators.required, Validators.email]],
-      signupmobile:['', [Validators.required, Validators.pattern("^[0-9]*$")] ],
-      password: ['', [Validators.required,Validators.minLength(6)]],
-      confirmPassword: ['', Validators.required]
-    }, {
-      validator: MustMatch('password', 'confirmPassword')
-    }
+    this.signupMenuzappform = this.formBuilder.group(
+      {
+        fname: ['', Validators.required],
+        lname: ['', Validators.required],
+        signupemail: [null, [Validators.required, Validators.email]],
+        signupmobile: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', Validators.required]
+      },
+      {
+        validator: MustMatch('password', 'confirmPassword')
+      }
     );
 
-    $(".toggle-password").click(function() {
-      $(this).toggleClass("fa-eye fa-eye-slash");
-      var input = $($(this).attr("toggle"));
-      if (input.attr("type") == "password") {
-        input.attr("type", "text");
+    // It is better to avoid the use of jquery in Angular apps. Follow this instead for a better solution
+    // https://dev.to/benneee_/creating-a-show-or-hide-password-feature-for-angular-forms-4fdk
+    $('.toggle-password').click(function() {
+      $(this).toggleClass('fa-eye fa-eye-slash');
+      var input = $($(this).attr('toggle'));
+      if (input.attr('type') == 'password') {
+        input.attr('type', 'text');
       } else {
-        input.attr("type", "password");
+        input.attr('type', 'password');
       }
     });
-
   }
 
-  get f() { return this.signupMenuzappform.controls;}
+  get f() {
+    return this.signupMenuzappform.controls;
+  }
 
   menuzappSignup() {
+    // Instead of using these flags, consider using the Angular's touch and dirty properties
+    // https://angular.io/guide/form-validation#validating-input-in-reactive-forms
     this.fnameSubmit = true;
     this.lnameSubmit = true;
     this.emailSubmit = true;
@@ -92,37 +107,37 @@ export class SignupComponent implements OnInit {
     if (this.signupMenuzappform.invalid) {
       return;
     }
-    if(this.signupMenuzappform.valid){
-      let data={
-        'first_name':this.signupMenuzappform.value.fname,
-        'last_name':this.signupMenuzappform.value.lname,
-        'email':this.signupMenuzappform.value.signupemail,
-        'mobile_number':this.signupMenuzappform.value.signupmobile,
-        'password':this.signupMenuzappform.value.password,
-        'confirm_password':this.signupMenuzappform.value.password,
-        'setPasswordURL':REQUEST_A_ACTIVE
-      }; 
+    if (this.signupMenuzappform.valid) {
+      let data = {
+        first_name: this.signupMenuzappform.value.fname,
+        last_name: this.signupMenuzappform.value.lname,
+        email: this.signupMenuzappform.value.signupemail,
+        mobile_number: this.signupMenuzappform.value.signupmobile,
+        password: this.signupMenuzappform.value.password,
+        confirm_password: this.signupMenuzappform.value.password,
+        setPasswordURL: REQUEST_A_ACTIVE
+      };
       this.alertservice.showLoader();
-      this.restApiservice.postAPI('signup-partner',data,(response)=>{
-        if(response && response['success'] && response['data']){         
+      this.restApiservice.postAPI('signup-partner', data, response => {
+        if (response && response['success'] && response['data']) {
           localStorage.setItem('email', this.signupMenuzappform.value.signupemail);
           // this.alertservice.Success(response['data']);
           this.alertservice.hideLoader();
           return this.router.navigateByUrl('/confirm-singup');
-        } else if(response && !response['success'] && response['error']['error']) {
-          let i=0;
-            for(let key in response['error']['error']) {
-              this.SignupcodeError = true;
-              this.errors[key]=response['error']['error'][key][0];
-              this.alertservice.showNotification(this.errors[key],'error');
-            }
+        } else if (response && !response['success'] && response['error']['error']) {
+          let i = 0;
+          for (let key in response['error']['error']) {
+            this.SignupcodeError = true;
+            this.errors[key] = response['error']['error'][key][0];
+            this.alertservice.showNotification(this.errors[key], 'error');
+          }
         } else {
-          this.alertservice.showNotification('Something went wrong','error');
+          this.alertservice.showNotification('Something went wrong', 'error');
         }
         this.alertservice.hideLoader();
       });
     } else {
-      this.alertservice.showNotification('Something went wrong','error');
+      this.alertservice.showNotification('Something went wrong', 'error');
       this.alertservice.hideLoader();
     }
   }
