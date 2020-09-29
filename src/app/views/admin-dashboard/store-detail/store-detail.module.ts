7849783374b107182d2ecc902e47594b8081d332
Ganesh Palankar <ganesh.p@sanradiance.com>
2020-09-29 14:08:05 +0530

    store profile in admin dashboard

--- src/app/views/admin-dashboard/store-detail/store-detail.module.ts
+++ src/app/views/admin-dashboard/store-detail/store-detail.module.ts
@@ -3,20 +3,40 @@ import { CommonModule } from '@angular/common';
 import { StorePendingDetailsComponent } from './admin-store-details/admin-store-details.component';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
+import { StoreMenuResolver } from 'src/app/_guards/store-menu-resolver';
+import { StoreDetailContainerComponent } from './store-detail-container/store-detail-container.component';
 
 const routes: Routes = [
   {
     path: ':id',
-    component: StorePendingDetailsComponent
+    resolve: { store: StoreMenuResolver },
+    component: StoreDetailContainerComponent,
+    children: [
+      {
+        path: 'approval',
+        component: StorePendingDetailsComponent,
+      },
+      {
+        path: 'profile',
+        loadChildren: () => import('../../dashboard/store-profile/store-profile.module').then(m => m.StoreProfileModule)
+      },
+      {
+        path: '**',
+        redirectTo: 'approval',
+        pathMatch: 'full'
+      },
+    ]
   }
+
 ]
 
 @NgModule({
-  declarations: [StorePendingDetailsComponent],
+  declarations: [StorePendingDetailsComponent, StoreDetailContainerComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
     SharedModule
-  ]
+  ],
+  providers: []
 })
 export class StoreDetailModule { }
