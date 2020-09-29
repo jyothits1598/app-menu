d82ae121d1cf14b965176715bfdbb61a31ecd236
arunkumartc <arunkumar.tc@sanradiance.com>
2020-09-29 09:15:49 +0530

    code upated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,17 +3,17 @@ export const environment = {
 
   // UAT server APIs
 
-  mail_url_success:'https://uat.menuzapp.com.au/email-verify',
-  mail_url_failure:'https://uat.menuzapp.com.au/email-token-expired',
-  mail_url_login:'https://uat.menuzapp.com.au/login',
-  mail_url_contactus:'https://uat.menuzapp.com.au/page-under-progress'
+  // mail_url_success:'https://uat.menuzapp.com.au/email-verify',
+  // mail_url_failure:'https://uat.menuzapp.com.au/email-token-expired',
+  // mail_url_login:'https://uat.menuzapp.com.au/login',
+  // mail_url_contactus:'https://uat.menuzapp.com.au/page-under-progress'
 
   // test server APIs
 
-  // mail_url_success:'http://54.252.119.115/email-verify',
-  // mail_url_failure:'http://54.252.119.115/email-token-expired',
-  // mail_url_login:'http://54.252.119.115/login',
-  // mail_url_contactus:'http://54.252.119.115/page-under-progress'
+  mail_url_success:'http://54.252.119.115/email-verify',
+  mail_url_failure:'http://54.252.119.115/email-token-expired',
+  mail_url_login:'http://54.252.119.115/login',
+  mail_url_contactus:'http://54.252.119.115/page-under-progress'
 
 };
 
@@ -21,10 +21,10 @@ export const environment = {
 //export const API_URL_LINK = 'http://3.7.155.134:81/';
 
  // UAT server APIs
-export const API_URL_LINK = 'https://uat.api.menuzapp.com/';
+// export const API_URL_LINK = 'https://uat.api.menuzapp.com/';
 
-// export const API_URL_LINK = 'http://54.252.119.115:8000/';
+export const API_URL_LINK = 'http://54.252.119.115:8000/';
 
-// export const REQUEST_A_ACTIVE = 'http://54.252.119.115/login';
+export const REQUEST_A_ACTIVE = 'http://54.252.119.115/login';
  
-export const REQUEST_A_ACTIVE = 'https://uat.menuzapp.com.au/login';
+// export const REQUEST_A_ACTIVE = 'https://uat.menuzapp.com.au/login';
